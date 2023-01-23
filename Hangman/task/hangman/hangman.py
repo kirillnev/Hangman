0792dd4088d01474330d817f4673d9837cabb4b5
Kirill Nevinskii <mail@kirillnev.ru>
2023-01-23 21:18:18 +0600

    Stage_5_8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,11 +1,41 @@
 import random
 
+
 hidden_words = ("python", "java", "swift", "javascript")
-free_symbol = "-"
+FREE_SYMBOL = "-"
 hidden_word = random.choice(hidden_words)
-hint = hidden_word[:3] + free_symbol * (len(hidden_word) - 3)
+uncovered_letters = set()
+MAX_ATTEMPTS = 8
 
-print("H A N G M A N")
+msg_start = "H A N G M A N"
+msg_input = "Input a letter: "
+mgs_false = "That letter doesn't appear in the word."
+msg_end = "Thanks for playing!"
 
-input_word = input(f"Guess the word {hint}: ")
-print("You survived!" if input_word == hidden_word else "You lost!")
+
+def hide_word(word, letters):
+    result = ""
+    for ch in word:
+        result += ch if ch in letters else FREE_SYMBOL
+    return result
+
+
+def go():
+    print(msg_start)
+    attempts = MAX_ATTEMPTS
+
+    while attempts > 0:
+        print("\n" + hide_word(hidden_word, uncovered_letters))
+        letter = input(msg_input)
+
+        if letter not in hidden_word:
+            print(mgs_false)
+
+        uncovered_letters.add(letter)
+        attempts -= 1
+
+    print("\n" + msg_end)
+
+
+if __name__ == "__main__":
+    go()
