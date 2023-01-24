11ae1c4c64eeb55d3097e6cfec8ed0e816c829b4
Kirill Nevinskii <mail@kirillnev.ru>
2023-01-24 09:53:32 +0600

    Stage_6_8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -9,8 +9,10 @@ MAX_ATTEMPTS = 8
 
 msg_start = "H A N G M A N"
 msg_input = "Input a letter: "
-mgs_false = "That letter doesn't appear in the word."
-msg_end = "Thanks for playing!"
+msg_false = "That letter doesn't appear in the word."
+msg_repeat = "No improvements."
+msg_win = "You guessed the word!\nYou survived!"
+msg_end = "You lost!"
 
 
 def hide_word(word, letters):
@@ -20,21 +22,35 @@ def hide_word(word, letters):
     return result
 
 
+def is_guess(word):
+    return FREE_SYMBOL not in word
+
+
+
 def go():
     print(msg_start)
     attempts = MAX_ATTEMPTS
-
-    while attempts > 0:
+    is_win = False
+    while attempts > 0 and not is_win:
         print("\n" + hide_word(hidden_word, uncovered_letters))
         letter = input(msg_input)
 
+        if letter in uncovered_letters:
+            print(msg_repeat)
+            attempts -= 1
         if letter not in hidden_word:
-            print(mgs_false)
+            print(msg_false)
+            attempts -= 1
 
         uncovered_letters.add(letter)
-        attempts -= 1
+        if is_guess(hide_word(hidden_word, uncovered_letters)):
+            is_win = True
 
-    print("\n" + msg_end)
+    if is_win:
+        print("\n" + hidden_word)
+        print(msg_win)
+    else:
+        print("\n" + msg_end)
 
 
 if __name__ == "__main__":
