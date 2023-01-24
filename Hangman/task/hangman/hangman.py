d6e18b4dab64fa1fded30c294ab6f5f9bdf26564
Kirill Nevinskii <mail@kirillnev.ru>
2023-01-24 13:45:25 +0600

    Stage_8_8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -7,6 +7,10 @@ FREE_SYMBOL = "-"
 hidden_word = random.choice(hidden_words)
 uncovered_letters = set()
 suggested_letters = set()
+results = {
+    "won": 0,
+    "lost": 0
+}
 
 MAX_ATTEMPTS = 8
 msg = {
@@ -16,9 +20,12 @@ msg = {
     "repeat": "You've already guessed this letter.",
     "long": "Please, input a single letter.",
     "invalid": "Please, enter a lowercase letter from the English alphabet.",
+    "invalid_command": "Please, enter a valid command.",
     "win": "You guessed the word {}!\nYou survived!",
     "unsolved": "Please, enter a lowercase letter from the English alphabet.",
-    "end": "You lost!"
+    "end": "You lost!",
+    "menu": "Type \"play\" to play the game, \"results\" to show the scoreboard, and \"exit\" to quit: ",
+    "statistics": "You won: {} times\nYou lost: {} times"
 }
 
 
@@ -34,7 +41,6 @@ def is_guess(word):
 
 
 def go():
-    print(msg["start"])
     attempts = MAX_ATTEMPTS
     is_win = False
     while attempts > 0 and not is_win:
@@ -62,9 +68,26 @@ def go():
     if is_win:
         print("\n" + hidden_word)
         print(msg["win"].format(hidden_word))
+        results["won"] += 1
     else:
         print("\n" + msg["end"])
+        results["lost"] += 1
 
 
 if __name__ == "__main__":
-    go()
+    print(msg["start"])
+    is_exit = False
+    while not is_exit:
+        command = input(msg["menu"])
+
+        if command == "exit":
+            is_exit = True
+        elif command == "play":
+            hidden_word = random.choice(hidden_words)
+            uncovered_letters = set()
+            suggested_letters = set()
+            go()
+        elif command == "results":
+            print(msg["statistics"].format(results["won"], results["lost"]))
+        else:
+            print(msg["invalid_command"])
