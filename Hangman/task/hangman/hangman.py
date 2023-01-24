9197108589a5e5ddf5434b38e1a8119dad9b7514
Kirill Nevinskii <mail@kirillnev.ru>
2023-01-24 13:01:48 +0600

    Stage_7_8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,18 +1,25 @@
 import random
+import string
 
 
 hidden_words = ("python", "java", "swift", "javascript")
 FREE_SYMBOL = "-"
 hidden_word = random.choice(hidden_words)
 uncovered_letters = set()
-MAX_ATTEMPTS = 8
+suggested_letters = set()
 
-msg_start = "H A N G M A N"
-msg_input = "Input a letter: "
-msg_false = "That letter doesn't appear in the word."
-msg_repeat = "No improvements."
-msg_win = "You guessed the word!\nYou survived!"
-msg_end = "You lost!"
+MAX_ATTEMPTS = 8
+msg = {
+    "start": "H A N G M A N",
+    "input": "Input a letter: ",
+    "false": "That letter doesn't appear in the word.",
+    "repeat": "You've already guessed this letter.",
+    "long": "Please, input a single letter.",
+    "invalid": "Please, enter a lowercase letter from the English alphabet.",
+    "win": "You guessed the word {}!\nYou survived!",
+    "unsolved": "Please, enter a lowercase letter from the English alphabet.",
+    "end": "You lost!"
+}
 
 
 def hide_word(word, letters):
@@ -26,31 +33,37 @@ def is_guess(word):
     return FREE_SYMBOL not in word
 
 
-
 def go():
-    print(msg_start)
+    print(msg["start"])
     attempts = MAX_ATTEMPTS
     is_win = False
     while attempts > 0 and not is_win:
         print("\n" + hide_word(hidden_word, uncovered_letters))
-        letter = input(msg_input)
+        letter = input(msg["input"])
 
-        if letter in uncovered_letters:
-            print(msg_repeat)
+        if len(letter) != 1:
+            print(msg["long"])
+        elif letter not in string.ascii_lowercase:
+            print(msg["invalid"])
+        elif letter in suggested_letters:
+            print(msg["repeat"])
             attempts -= 1
-        if letter not in hidden_word:
-            print(msg_false)
+        elif letter not in hidden_word:
+            print(msg["false"])
             attempts -= 1
+            suggested_letters.add(letter)
+        else:
+            uncovered_letters.add(letter)
+            suggested_letters.add(letter)
 
-        uncovered_letters.add(letter)
         if is_guess(hide_word(hidden_word, uncovered_letters)):
             is_win = True
 
     if is_win:
         print("\n" + hidden_word)
-        print(msg_win)
+        print(msg["win"].format(hidden_word))
     else:
-        print("\n" + msg_end)
+        print("\n" + msg["end"])
 
 
 if __name__ == "__main__":
